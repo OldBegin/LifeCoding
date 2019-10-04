e70576000a2f48b7ac99faab726422e86d2b5975
youngunyou <ygyou@me.com>
2019-10-04 09:34:30 +0800

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 var app = express();
 
@@ -10,9 +11,12 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/www.unitedin.kr/fullchain.pem')
 };
 
-
 app.get('/', function (req, res) {
-  res.send('Hello World3000!');
+  if (url.parse(req.url).protocol == 'https:') {
+    res.send("connected from https:")
+  }else {
+    res.send("connected from http:")
+  }
 });
 
 app.get('/insecure', function (req, res) {
@@ -20,8 +24,8 @@ app.get('/insecure', function (req, res) {
 });
 
 http.createServer(app).listen(80, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 80!');
 });
 https.createServer(options,app).listen(443, function () {
-  console.log('Example app listening on port 3001!');
+  console.log('Example app listening on port 443!');
 });
