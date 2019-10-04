9a92600a2b8d3909d2a669a5933927f44aeddce4
youngunyou <ygyou@me.com>
2019-10-04 14:34:01 +0800

    none

--- server.js
+++ server.js
@@ -11,23 +11,26 @@ var app = express();
 //   key: fs.readFileSync('./openSSLcert/key.pem'),
 //   cert: fs.readFileSync('./openSSLcert/key-cert.pem')
 // };
-//////////////  서버용 인증서 ///////////////////////////////////////////
+////////////  서버용 인증서 ///////////////////////////////////////////
 const options = {
   key: fs.readFileSync('/etc/letsencrypt/live/www.unitedin.kr/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/www.unitedin.kr/cert.pem')
 };
 
 app.get('/', function (req, res) {
-  if (url.parse(req.url).protocol == 'https:') {
+  if (url.parse(req.url).protocol == 'https') {
     res.send("connected from https:")
   }else {
     res.send("connected from http:")
   }
+  console.log('protocol: ' + url.parse(req.url).protocol);
+  console.log('pathname: ' + url.parse(req.url).pathname);
+  console.log('host: ' + url.parse(req.url).host);
 });
 
 http.createServer(app).listen(80, function () {
   console.log('Example app listening on port 80!');
 });
-https.createServer(options,app).listen(443, function () {
-  console.log('Example app listening on port 3001!');
+https.createServer(app).listen(443, function () {
+  console.log('Example app listening on port 443!');
 });
