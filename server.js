11bf6a44e92c5586a104263cf1f811ccef271b44
youngunyou <ygyou@me.com>
2019-10-04 09:09:47 +0800

    server.js default setting

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
+var app = express();
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/www.unitedin.kr/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/www.unitedin.kr/fullchain.pem')
+};
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World3000!');
+});
+
+app.get('/insecure', function (req, res) {
+  res.send('Dangerous!');
+});
+
+http.createServer(app).listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+https.createServer(option,app).listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
